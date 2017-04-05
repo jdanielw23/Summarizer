71fe62e378f52d49e7d0bc040c4d0ef528d75c66
Andrew Shubin <andrew.shubin@calbaptist.edu>
2017-04-05 01:19:17 -0700

    (Andrew) Still trying to fix things...

---
+++ Summarizer/Model/Daniels Implementation/SummarizerDW.cs
@@ -0,0 +1,317 @@
+﻿using Summarizer.Model.Utils.Stemming;
+using Summarizer.Model.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Summarizer.Model.Daniels_Implementation
+{
+    /// <summary>
+    /// Created by J. Daniel Worthington
+    /// Contains the main logic for summarizing a document
+    /// </summary>
+    public class SummarizerDW : SummarizerImplementation
+    {
+        private int MinSentenceLength;
+        private int MaxSentenceLength;
+        private int MinWordLength;
+
+        /*********************************************
+        FUTURE STEPS:
+        I somewhat like the current results.        
+        
+        BUT....
+
+        Possible Implementation Enhancements:
+            -Maybe instead of just using the frequency of each word, try to account for words
+             that occur too frequently (Mean, StdDev).
+            -Maybe just take the sentence before and after the highest scoring sentence?
+            -Maybe create regex that will separate the bible text by verse or sentence,
+                whichever is most complete.
+            -Include Part of Speech tagging to narrow down word definitions
+        **********************************************/
+
+        public SummarizerDW(int minWordLength = 3, int minSentenceLength = 8,
+            int maxSentenceLength = 50)
+        {
+            MinWordLength = minWordLength;
+            MinSentenceLength = minSentenceLength;
+            MaxSentenceLength = maxSentenceLength;
+        }
+
+        /// <summary>
+        /// Algorithm 2: Uses sentence score and thesaurus to look for similar keys
+        /// </summary>
+        /// <param name="originalText">The original text to be summarized</param>
+        /// <returns>The summary of the document</returns>
+        public string Summarize(string originalText)
+        {
+            Summary summary = new Summary(MinSentenceLength, MaxSentenceLength);
+
+            // Break the text into sentences
+            string[] sentences = SplitIntoSentences(originalText);
+
+            // Generate a word frequency matrix based on words that are similar to each other in meaning
+            FrequencyMatrix wordFrequency = new FrequencyMatrix();
+            for (int sentenceIndex = 0; sentenceIndex < sentences.Length; sentenceIndex++)
+            {
+                StringBuilder key = new StringBuilder();
+                foreach (string rawWord in sentences[sentenceIndex].Split(' '))
+                {
+                    string word = SimplifyWord(rawWord, false);
+
+                    if (IsValidWord(word, true, true, true, false))
+                    {
+                        wordFrequency.AddToMatrix(word, sentenceIndex);
+                    }
+                }
+            }
+
+            // Score each sentence by summing the word frequency of each word in the sentence
+            // and dividing it by the total number of words in the sentence
+            for (int sentenceIndex = 0; sentenceIndex < sentences.Length; sentenceIndex++)
+            {
+                int sum = 0;
+                int numWords = 0;
+                foreach (string rawWord in sentences[sentenceIndex].Split(' '))
+                {
+                    string word = SimplifyWord(rawWord, false);
+                    numWords++;
+                    sum += wordFrequency[word].Frequency;
+                }
+                double score = (numWords == 0) ? 0 : (sum / numWords);
+
+                // The Summary data structure will only hold the 3 highest scoring sentences
+                summary.AddToSummary(new SentenceScore()
+                {
+                    Sentence = sentences[sentenceIndex].Trim().Capitalize(),
+                    Score = score
+                });
+            }
+
+            return summary.ToString();
+        }
+
+        /// <summary>
+        /// Deprecated: No longer using this implementation
+        /// Algorithm 1: Creates a bigram and returns top 3 sentences
+        /// </summary>
+        /// <param name="filePath">The full path to the file to be summarized</param>
+        /// <returns>The summary of the document</returns>
+        public string SummarizeDocumentMethod1(string filePath)
+        {
+            // Read in the text
+            string text = System.IO.File.ReadAllText(filePath);
+
+            // Break the text into sentences
+            string[] sentences = SplitIntoSentences(text);
+
+            // This is the data structure for my bigram
+            Bigram wordFrequency = new Bigram();
+
+            // Iterate through each sentence
+            for (int sentenceIndex = 0; sentenceIndex < sentences.Length; sentenceIndex++)
+            {
+                string prevWord = "";
+                // Break each sentence into words
+                foreach (string rawWord in sentences[sentenceIndex].Split(' '))
+                {
+                    // Simplify word to its simplest form
+                    string word = SimplifyWord(rawWord, true);
+
+                    // Disregard stop words and words shorter than MIN_WORD_LENGTH
+                    if (IsValidWord(word, true, true, true, false))
+                    {
+                        wordFrequency.AddToMatrix(prevWord, word, sentenceIndex);
+                        prevWord = word;
+                    }
+                }
+            }
+
+            // Create a simpler word frequency with the two words combined together
+            IDictionary<string, FrequencyLocation> finalWordFrequency = wordFrequency
+                                    .FinalWordFrequency;
+
+            // FURTHER ANALYSIS:
+            // Account for word frequencies that are too common?
+
+            /****    Cross-reference sentence indexes in most common word pairs
+            IList<int> commonIndexes = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                foreach(int index in finalWordFrequency.OrderByDescending(kv => kv.Value
+                                .Frequency).ElementAt(i).Value.Locations.Intersect(
+                finalWordFrequency.OrderByDescending(kv => kv.Value.Frequency)
+                                .ElementAt(i + 1).Value.Locations))
+                {
+                    if (!commonIndexes.Contains(index))
+                        commonIndexes.Add(index);
+                }                
+            }
+            /****/
+
+            // Build summary based on results
+            // Options:
+            //  First three sentences with top word frequency
+            //  First sentence from each of top 3 word frequencies
+            //  etc...
+
+            StringBuilder summary = new StringBuilder();
+
+            /****    OPTION 0: Show common Indexes    ****
+            int count = 0;
+            foreach(int i in commonIndexes)
+            {
+                summary.AppendLine(sentences[i]);
+                count++;
+                // Limit the number of sentences to 3
+                if (count == 3)
+                    break;
+            }
+            /****/
+
+            /****    OPTION 1: This will print the word frequencies    ****
+            foreach (var pair in finalWordFrequency.OrderByDescending(kv => kv.Value.Frequency))
+            {
+                //// This will write the word pair frequencies, number of sentences,
+                //  and the list of sentence indexes
+                summary.AppendLine(string.Format("{0}: {1}, Num sentences: {2}; {3}\n", 
+                    pair.Key, pair.Value.Frequency, 
+                    pair.Value.Locations.Count, 
+                    pair.Value.Locations.ListData(", ")));
+            }
+            /****/
+
+
+            /****    OPTION 2: This will print the top 3 or fewer sentences containing
+             *  the most common word pair    ****
+            var mostCommonPair = finalWordFrequency.OrderByDescending(kv => kv.Value.Frequency)
+                                        .ElementAt(0);
+            int count = 0;
+            foreach (int location in mostCommonPair.Value.Locations)
+            {
+                summary.AppendLine(sentences[location]);
+                count++;
+                if (count == 3)
+                    break;
+            }
+            /****/
+
+            /****    OPTION 3: This will print the first sentence from the top 3 pairs    ****
+            int count = 0;
+            int prevSent = 0;
+            foreach (var pair in finalWordFrequency.OrderByDescending(kv => kv.Value.Frequency))
+            {
+                int sent = pair.Value.Locations[0];
+                if (prevSent != sent)
+                {
+                    summary.AppendLine(sentences[sent]);
+                    count++;
+                }
+
+                if (count == 3)
+                    break;
+
+                prevSent = sent;
+            }
+            /****/
+
+            return summary.ToString();
+        }
+
+        /// <summary>
+        /// Summarizes the specified document and writes it to a new file
+        /// </summary>
+        /// <param name="filePath">File to be summarized</param>
+        /// <param name="newFilePath">File to be created from summary</param>
+        public void SummarizeToNewDocument(string filePath, string newFilePath)
+        {
+            string summary = Summarize(filePath);
+            System.IO.File.WriteAllText(newFilePath, summary);
+        }
+
+        /*****************************************************/
+        /***************    PRIVATE METHODS    ***************/
+        /*****************************************************/
+
+        /// <summary>
+        /// Trims punctuation and spacing off of the supplied word and if specified,
+        /// also stems the word
+        /// </summary>
+        /// <param name="word">The word to be simplified</param>
+        /// <param name="stemWord">If true, stems the word</param>
+        /// <returns>The trimmed word</returns>
+        private string SimplifyWord(string word, bool stemWord)
+        {
+            string trimmedWord = word.Trim().ToLower().TrimEnd(',', ':', ';', '.', '!', '?', 's');
+
+            if (stemWord)
+                trimmedWord = new EnglishStemmer().Stem(trimmedWord);
+
+            return trimmedWord;
+        }
+
+        /// <summary>
+        /// Using various options, checks to see if the supplied word is valid
+        /// </summary>
+        /// <param name="word">Word to check</param>
+        /// <param name="minWordLength">If true, returns false if word.Length is less
+        /// than MinWordLength</param>
+        /// <param name="lettersOnly">If true, returns false is words contains
+        /// anything other than letters</param>
+        /// <param name="noStopWords">If true, returns false if the word is a stop word</param>
+        /// <param name="noVerbs">If true, returns false if word is a verb
+        /// NOTE: Not yet implemented</param>
+        private bool IsValidWord(string word, bool minWordLength, bool lettersOnly,
+            bool noStopWords, bool noVerbs)
+        {
+            if (minWordLength)
+            {
+                if (word.Length < MinWordLength)
+                    return false;
+            }
+
+            if (lettersOnly)
+            {
+                if (!word.ContainsOnlyLetters())
+                    return false;
+            }
+
+            if (noStopWords)
+            {
+                if (Constants.BibleStopWordList.Contains(word))
+                    return false;
+            }
+
+            if (noVerbs)
+            {
+                // Not yet implemented
+                // Probably not necessary
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Splits the text of the document into an array of sentences.
+        /// </summary>
+        /// <param name="text">The text to be split</param>
+        /// <returns>An array of sentences</returns>
+        public static string[] SplitIntoSentences(string text)
+        {
+            string pattern = @"[^\.\?\!]*[\.\?\!]";
+            IList<string> result = new List<string>();
+
+            foreach (var match in Regex.Matches(text, pattern))
+            {
+                result.Add(match.ToString());
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ SummarizerTest/ModelTest/SummarizerDWTest.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Summarizer.Model;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using Summarizer.Model.Daniels_Implementation;
+using Summarizer.Model.Utils.Stemming;
+using Summarizer.Model.Utils;
+
+namespace SummarizerTest.ModelTest
+{
+    /// <summary>
+    /// Created by J. Daniel Worthington
+    /// A class to test Daniel's summarization implementation
+    /// </summary>
+    [TestClass]
+    public class SummarizerDWTest
+    {
+        [TestMethod]
+        public void SummarizeAllDocumentsTest()
+        {
+            const int NUM_BOOKS = 16;
+            int count = 0;
+            foreach (string path in Directory.EnumerateFiles(@"..\..\..\Summarizer\Documents\The Bible txt - Original"))
+            {
+                FileInfo file = new FileInfo(path);
+                if (file.Extension.Equals(".txt"))
+                {
+                    count++;
+                    string newFilePath = @"..\..\..\Summarizer\Documents\The Bible txt - Summarized\" + file.Name.Split('.')[0] + "_s" + file.Extension;
+                    SummarizerDW summarizer = new SummarizerDW();
+                    summarizer.SummarizeToNewDocument(path, newFilePath);
+                }
+                //if (count > NUM_BOOKS)
+                //    break;
+            }
+        }
+
+        [TestMethod]
+        public void SplitIntoSentencesTest()
+        {
+            string text =
+                "1:8 And God called the firmament Heaven. And the evening and the morning were the second day. " +
+                "1:9 And God said, Let the waters under the heaven be gathered together " +
+                "unto one place, and let the dry land appear: and it was so. " +
+                "1:10 And God called the dry land Earth; and the gathering together of " +
+                "the waters called he Seas: and God saw that it was good. " +
+                "1:11 And God said, Let the earth bring forth grass, the herb yielding " +
+                "seed, and the fruit tree yielding fruit after his kind, whose seed is " +
+                "in itself, upon the earth: and it was so. " +
+                "1:12 And the earth brought forth grass, and herb yielding seed after " +
+                "his kind, and the tree yielding fruit, whose seed was in itself, after " +
+                "his kind: and God saw that it was good. " +
+                "1:13 And the evening and the morning were the third day. " +
+                "1:14 And God said, Let there be lights in the firmament of the heaven " +
+                "to divide the day from the night; and let them be for signs, and for " +
+                "seasons, and for days, and years: " +
+                "1:15 And let them be for lights in " +
+                "the firmament of the heaven to give light upon the earth: and it was so. " +
+                "1:16 And God made two great lights; the greater light to rule the day, " +
+                "and the lesser light to rule the night: he made the stars also? " +
+                "1:17 And God set them in the firmament of the heaven to give light " +
+                "upon the earth, " +
+                "1:18 And to rule over the day and over the night, and " +
+                "to divide the light from the darkness: and God saw that it was good. " +
+                "1:19 And the evening and the morning were the fourth day!";
+
+            string[] sentences = SummarizerDW.SplitIntoSentences(text);
+            Assert.AreEqual(11, sentences.Length);
+        }
+
+        [TestMethod]
+        public void StemmerTest()
+        {
+            string[] maintain = new string[] { "maintain", "maintains", "maintaining", "maintained" };
+            string[] walk = new string[] { "walk", "walking", "walked", "walks" };
+
+            IStemmer stemmer = new EnglishStemmer();
+
+            foreach (string word in maintain)
+            {
+                Assert.AreEqual("maintain", stemmer.Stem(word));
+            }
+
+            foreach (string word in walk)
+            {
+                Assert.AreEqual("walk", stemmer.Stem(word));
+            }
+
+        }
+
+        [TestMethod]
+        public void TrimCharactersTest()
+        {
+            string[] toTrim = new string[] { "furnace", "furnace,", "furnace;", "furnace:" };
+
+            foreach (string word in toTrim)
+            {
+                Assert.AreEqual("furnace", word.TrimEnd(',', ':', ';'));
+            }
+        }
+
+
+        /****    ONE TIME SCRIPTS    ****
+        
+        [TestMethod]
+        public void FixTxtFiles()
+        {
+            foreach (string path in Directory.EnumerateFiles(@"D:\My Libraries\My Documents\The Bible txt"))
+            {
+                FileInfo file = new FileInfo(path);
+                if (file.Extension.Equals(".txt"))
+                {
+                    string newFilePath = @"D:\My Libraries\My Documents\The Bible txt\Fixed\" + file.Name;
+
+                    StringBuilder newText = new StringBuilder();
+                    StringBuilder lines = new StringBuilder();
+
+                    string text = System.IO.File.ReadAllText(path);
+                    bool first = true;
+                    foreach (string line in text.Split('\r'))
+                    {
+                        lines.Append(line.Trim()).Append(" ");
+                        if (first)
+                        {
+                            newText.AppendLine(line);
+                            first = false;
+                        }
+                    }
+
+                    string pattern = @"[\d]+:[\d]+[\D]*";
+                    IList<string> verses = new List<string>();
+                    foreach (var match in Regex.Matches(lines.ToString(), pattern))
+                    {
+                        verses.Add(match.ToString());
+                    }
+
+                    foreach (string verse in verses)
+                    {
+                        newText.AppendLine(verse);
+                    }
+
+                    System.IO.File.WriteAllText(newFilePath, newText.ToString());
+                }
+            }
+        }
+        /*******************/
+
+        /***************
+        [TestMethod]
+        public void SeparateBible()
+        {
+            string biblePath = @"D:\My Libraries\My Documents\The Bible txt\Bible_KJV.txt";
+
+            string[] bibleText = System.IO.File.ReadAllLines(biblePath);
+
+            IList<string> books = new List<string>();
+            
+            int newLineCount = 0;
+            StringBuilder bk = new StringBuilder();
+            foreach (string nxtLine in bibleText)
+            {
+                if (nxtLine.Equals(""))
+                    newLineCount++;
+                else
+                    newLineCount = 0;
+
+                if (newLineCount < 4)
+                    bk.AppendLine(nxtLine);
+                else
+                {
+                    books.Add(bk.ToString());
+                    bk.Clear();
+                    newLineCount = 0;
+                }
+            }
+
+            IList<string> bookPaths = new List<string>();
+            int bookNum = 1;
+            foreach (string book in books)
+            {
+                if (book.Length < 100)
+                    continue;
+
+                string bookName = book.Split('\n')[0].Trim();
+                if (bookName.Contains(":"))
+                    bookName = bookName.Split(' ').Last().Trim();
+
+                string bookPath = string.Format(@"D:\My Libraries\My Documents\The Bible txt\{0:00} - {1}.txt", bookNum, bookName);
+
+                System.IO.File.WriteAllText(bookPath, book);
+                bookNum++;
+                //bookPaths.Add(bookPath);
+            }
+
+            //System.IO.File.WriteAllLines(@"D:\My Libraries\My Documents\The Bible txt\BookPaths.txt", bookPaths);
+        }
+        /****/
+    }
+}
